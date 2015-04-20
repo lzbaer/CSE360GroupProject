f9dffca596228c94d5c5d86881681697b6e8c3ad
wi1lywonka <michaelsenpatrick@gmail.com>
2015-04-20 00:20:05 -0700

    addded login, splash GUI components

--- app/src/main/java/com/medipal/Login.java
+++ app/src/main/java/com/medipal/Login.java
@@ -1,17 +1,116 @@
 package com.medipal;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 public class Login extends ActionBarActivity {
+
+    /**
+     * A dummy authentication store containing known user names and passwords.
+     * TODO: remove after connecting to a real authentication system.
+     */
+    private static final String[] DUMMY_CREDENTIALS = new String[]{
+            "foo@example.com:hello", "bar@example.com:world",
+            "michaelsenpatrick@gmail.com:blondie", "user@test.com:password"
+    };
+
+    /**
+     * Keep track of the login task to ensure we can cancel it if requested.
+     */
+    //private UserLoginTask mAuthTask = null;
+
+    //UI elements
+    private AutoCompleteTextView mEmailView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mEmailLoginFormView;
+    private View mSignOutButtons;
+    private View mLoginFormView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Set up the login form.
+        mEmailView = (AutoCompleteTextView) findViewById(R.id.email);
+        // TODO populateAutoComplete();
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        //create button & action listener for signing in
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+    }
+
+    public void attemptLogin()
+    {
+        //TODO
+        //TODO remove this login system which does not check for credentials
+        // Reset errors.
+        mEmailView.setError(null);
+        mPasswordView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmailView.getText().toString();
+        String password = mPasswordView.getText().toString();
+        System.out.print(email);
+        boolean cancel = false;
+        View focusView = null;
+
+        //TODO FINISH IMPLEMENTING PASSWORD/EMAIL VALIDITY CHECK
+        //CURRENTLY ONLY CHECKS FOR EMPTY EMAIL, NEEDS TO CHECK FOR
+        //INVALID EMAIL, EMPTY PASSWORD, INVALID PASSWORD
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmailView.setError(getString(R.string.error_field_required));
+            focusView = mEmailView;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+            //TODO LEARN HOW TO USE MAUTH OBJECT TO AUTHORIZE USERS
+            //start splash page for either doctor or patient
+            Intent intent = null;
+            if (email.compareToIgnoreCase("patient")==0) {
+                intent = new Intent(this, PatientSplash.class);
+            } else if (email.compareToIgnoreCase("doctor")==0) {
+                intent = new Intent(this, DoctorSplash.class);
+            }
+            if (intent != null) {
+                startActivity(intent);
+            }
+        }
     }
 
 
