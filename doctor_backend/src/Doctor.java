c3291d0af4f57504e1cc4e0c06fd0f838ab223a8
rseto857 <rseto@asu.edu>
2015-04-20 23:52:00 -0700

    This is the doctor_backend, first version

---
+++ doctor_backend/src/Doctor.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+@SuppressWarnings("unused")
+public class Doctor { //extends User
+	protected static int doctorID;
+	//private ArrayList <Patient> patientList = new ArrayList<Patient>();
+	private int[] urgencyOrder;
+	
+	/*
+	// Constructor to create a doctor account
+	public Doctor (String user, String pass) {
+		***CHECK DATABASE FOR ALREADY EXISTING USERNAME***
+		userName = user;
+		password = pass;
+		
+		id = ++nextDoctorID;
+	}*/
+	
+	public Patient[] getPatients() {
+		
+	}
+}
---
+++ doctor_backend/src/PatientList.java
@@ -0,0 +1,19 @@
+
+@SuppressWarnings("unused")
+public class PatientList extends Doctor{
+	static int mockPatientID = 0;
+	
+	public static void main (String[] args) {
+		displayList(doctorID);
+		displayPatientInfo(mockPatientID);			
+	}
+	
+	public static void displayList(int doctorID) {
+		// SEARCH DATABASE FOR DOCTOR ID and then do doctor.getPatients()
+	}
+	
+	public static void displayPatientInfo (int patientID) {
+		// patientID.getRecord();
+	}
+	
+}
