1bfa038e61cb054413b107500c966a969cbd79cb
lzbaer <lzbaer@asu.edu>
2015-04-28 02:17:47 -0700

    Modified urgency and similarity files

--- Patient_backend/patient_backend/src/patient_backend/SimilarityCalculation.java
+++ Patient_backend/patient_backend/src/patient_backend/SimilarityCalculation.java
@@ -1,5 +1,22 @@
 package patient_backend;
 
 public class SimilarityCalculation {
+	//find average of each symptom
+	int numIllnesses = 20;
+	double[][] previousSymptomRatings;
+	double[] averageRatings;
+	
+	public void method(){
+		for(int i=0;i<numIllnesses;i++){
+			for(int j=0;j<9;j++){
+				averageRatings[j]+= getPatientSymptomRating(j);
+			}
+		}
+	}
 
+	private double getPatientSymptomRating(int j) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
 }
